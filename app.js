501be1176e71834a15a2e47dee1574c503598e12
nurfitrys <nurftrys@gmail.com>
2019-09-20 14:27:51 +0700

    nurfitrys

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', routes)
+app.use('/images', routes)
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
